9f76197d4dbf87473baa56a58785cfe218246bcd
Sharjith Naramparambath <sharjith@gmail.com>
2020-08-03 16:52:11 +0530

    Use at method instead of subscript for exception handling

--- MeshProperties.cpp
+++ MeshProperties.cpp
@@ -43,9 +43,9 @@ void MeshProperties::calculateSurfaceAreaAndVolume()
     try {
         for (size_t i = 0; i < _meshPoints.size(); i += 9)
         {
-            QVector3D p1(_meshPoints[i + 0], _meshPoints[i + 1], _meshPoints[i + 2]);
-            QVector3D p2(_meshPoints[i + 3], _meshPoints[i + 4], _meshPoints[i + 5]);
-            QVector3D p3(_meshPoints[i + 6], _meshPoints[i + 7], _meshPoints[i + 8]);
+            QVector3D p1(_meshPoints.at(i + 0), _meshPoints.at(i + 1), _meshPoints.at(i + 2));
+            QVector3D p2(_meshPoints.at(i + 3), _meshPoints.at(i + 4), _meshPoints.at(i + 5));
+            QVector3D p3(_meshPoints.at(i + 6), _meshPoints.at(i + 7), _meshPoints.at(i + 8));
 
             _volume += QVector3D::dotProduct(p1, (QVector3D::crossProduct(p2, p3))) / 6.0f;
 
