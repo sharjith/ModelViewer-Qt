7ab3e3ddb7db80b216946b91d4218b9468075e88
Sharjith Naramparambath <sharjith@gmail.com>
2020-08-09 22:33:53 +0530

    Modified intersectsWithRay

--- ParametricSurface.cpp
+++ ParametricSurface.cpp
@@ -111,8 +111,8 @@ void ParametricSurface::buildMesh()
 	idx = 0;
 	for (GLuint i = 0; i < _slices; i++)
 	{
-		GLuint stackStart = i * (_stacks + 1);
-		GLuint nextStackStart = (i + 1) * (_stacks + 1);
+        GLuint stackStart = i * (_stacks + 1);
+        GLuint nextStackStart = (i + 1) * (_stacks + 1);
 		for (GLuint j = 0; j < _stacks; j++)
 		{
 			// For quad mesh
@@ -132,34 +132,17 @@ void ParametricSurface::buildMesh()
 bool ParametricSurface::intersectsWithRay(const QVector3D& rayPos, const QVector3D& rayDir, QVector3D& outIntersectionPoint)
 {
 	bool intersects = false;
-	
-	GLuint idx = 0;
-	for (GLuint i = 0; i < _slices; i++)
-	{
-		GLuint stackStart = i * (_stacks + 1);
-		GLuint nextStackStart = (i + 1) * (_stacks + 1);
-		for (GLuint j = 0; j < _stacks; j++)
-		{
-			QVector3D v0(_trsfpoints[stackStart + j + 0], _trsfpoints[stackStart + j + 1], _trsfpoints[stackStart + j + 2]);
-			QVector3D v1(_trsfpoints[stackStart + j + 1 + 3], _trsfpoints[stackStart + j + 1 + 4], _trsfpoints[stackStart + j + 1 + 5]);
-			QVector3D v2(_trsfpoints[nextStackStart + j + 1 + 6], _trsfpoints[nextStackStart + j + 1 + 7], _trsfpoints[nextStackStart + j + 1 + 8]);
 
-			intersects = rayIntersectsTriangle(rayPos, rayDir, v0, v1, v2, outIntersectionPoint);
-			if (intersects)
-				break;
-
-			QVector3D v3(_trsfpoints[stackStart + j + 0], _trsfpoints[stackStart + j + 1], _trsfpoints[stackStart + j + 2]);
-			QVector3D v4(_trsfpoints[stackStart + j + 1 + 3], _trsfpoints[stackStart + j + 1 + 4], _trsfpoints[stackStart + j + 1 + 5]);
-			QVector3D v5(_trsfpoints[nextStackStart + j + 9], _trsfpoints[nextStackStart + j + 10], _trsfpoints[nextStackStart + j + 11]);
-
-			intersects = rayIntersectsTriangle(rayPos, rayDir, v3, v4, v5, outIntersectionPoint);
-			if (intersects)
-				break;
-			
-			
-			idx += 4;
-		}
-	}
+    int offset = _stacks * 3;
+    for(size_t i = 0; i < _trsfpoints.size(); i++)
+    {
+        QVector3D v0(_trsfpoints[i + 0], _trsfpoints[i + 1], _trsfpoints[i + 2]);
+        QVector3D v1(_trsfpoints[i + 3], _trsfpoints[i + 4], _trsfpoints[i + 5]);
+        QVector3D v2(_trsfpoints[offset + i + 0], _trsfpoints[offset + i + 1], _trsfpoints[offset + i + 2]);
+        intersects = rayIntersectsTriangle(rayPos, rayDir, v0, v1, v2, outIntersectionPoint);
+        if (intersects)
+            break;
+    }
 
 	return intersects;
 }
