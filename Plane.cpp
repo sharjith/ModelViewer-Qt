93f0420a6700e3f68e45d7d10a262b7e04b6fc13
Sharjith Naramparambath <sharjith@gmail.com>
2020-08-11 20:08:52 +0530

    Added method setPlane

--- Plane.cpp
+++ Plane.cpp
@@ -3,32 +3,32 @@
 #include <cstdio>
 #include <cmath>
 
-Plane::Plane(QOpenGLShaderProgram* prog, float xsize, float zsize, int xdivs, int zdivs, float smax, float tmax):
+Plane::Plane(QOpenGLShaderProgram* prog, float xsize, float ysize, int xdivs, int ydivs, float zlevel, float smax, float tmax):
 	TriangleMesh(prog, "Plane")
 {
-    std::vector<GLfloat> p(3 * (xdivs + 1) * (zdivs + 1));
-	std::vector<GLfloat> n(3 * (xdivs + 1) * (zdivs + 1));
-    std::vector<GLfloat> tex(2 * (xdivs + 1) * (zdivs + 1));
-    std::vector<GLuint> el(6 * xdivs * zdivs);
+    std::vector<GLfloat> p(3 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLfloat> n(3 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLfloat> tex(2 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLuint> el(6 * xdivs * ydivs);
 
     float x2 = xsize / 2.0f;
-    float z2 = zsize / 2.0f;
-    float iFactor = (float)zsize / zdivs;
+    float y2 = ysize / 2.0f;
+    float iFactor = (float)ysize / ydivs;
     float jFactor = (float)xsize / xdivs;
-    float texi = smax / zdivs;
+    float texi = smax / ydivs;
     float texj = tmax / xdivs;
-    float x, z;
+    float x, y;
     int vidx = 0, tidx = 0;
-    for( int i = 0; i <= zdivs; i++ ) {
-        z = iFactor * i - z2;
+    for( int i = 0; i <= ydivs; i++ ) {
+        y = iFactor * i - y2;
         for( int j = 0; j <= xdivs; j++ ) {
             x = jFactor * j - x2;
             p[vidx] = x;
-            p[vidx+1] = 0.0f;
-            p[vidx+2] = z;
+            p[vidx+1] = y;
+            p[vidx+2] = zlevel;
 			n[vidx] = 0.0f;
-			n[vidx+1] = 1.0f;
-			n[vidx+2] = 0.0f;
+            n[vidx+1] = 0.0f;
+            n[vidx+2] = -1.0f;
 
             tex[tidx] = j * texi;
             tex[tidx+1] = i * texj;
@@ -40,7 +40,60 @@ Plane::Plane(QOpenGLShaderProgram* prog, float xsize, float zsize, int xdivs, in
 
     GLuint rowStart, nextRowStart;
     int idx = 0;
-    for( int i = 0; i < zdivs; i++ ) {
+    for( int i = 0; i < ydivs; i++ ) {
+        rowStart = (GLuint)( i * (xdivs+1) );
+        nextRowStart = (GLuint)( (i+1) * (xdivs+1));
+        for( int j = 0; j < xdivs; j++ ) {
+            el[idx] = rowStart + j;
+            el[idx+1] = nextRowStart + j;
+            el[idx+2] = nextRowStart + j + 1;
+            el[idx+3] = rowStart + j;
+            el[idx+4] = nextRowStart + j + 1;
+            el[idx+5] = rowStart + j + 1;
+            idx += 6;
+        }
+    }
+
+    initBuffers(&el, &p, &n, &tex);
+}
+
+void Plane::setPlane(QOpenGLShaderProgram* prog, float xsize, float ysize, int xdivs, int ydivs, float zlevel, float smax, float tmax)
+{
+    std::vector<GLfloat> p(3 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLfloat> n(3 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLfloat> tex(2 * (xdivs + 1) * (ydivs + 1));
+    std::vector<GLuint> el(6 * xdivs * ydivs);
+
+    float x2 = xsize / 2.0f;
+    float y2 = ysize / 2.0f;
+    float iFactor = (float)ysize / ydivs;
+    float jFactor = (float)xsize / xdivs;
+    float texi = smax / ydivs;
+    float texj = tmax / xdivs;
+    float x, y;
+    int vidx = 0, tidx = 0;
+    for( int i = 0; i <= ydivs; i++ ) {
+        y = iFactor * i - y2;
+        for( int j = 0; j <= xdivs; j++ ) {
+            x = jFactor * j - x2;
+            p[vidx] = x;
+            p[vidx+1] = y;
+            p[vidx+2] = zlevel;
+            n[vidx] = 0.0f;
+            n[vidx+1] = 0.0f;
+            n[vidx+2] = -1.0f;
+
+            tex[tidx] = j * texi;
+            tex[tidx+1] = i * texj;
+
+            vidx += 3;
+            tidx += 2;
+        }
+    }
+
+    GLuint rowStart, nextRowStart;
+    int idx = 0;
+    for( int i = 0; i < ydivs; i++ ) {
         rowStart = (GLuint)( i * (xdivs+1) );
         nextRowStart = (GLuint)( (i+1) * (xdivs+1));
         for( int j = 0; j < xdivs; j++ ) {
