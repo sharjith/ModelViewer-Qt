c03b3c95c79df9696cfb452e68bdf1d9615ccbbc
Sharjith Naramparambath <sharjith@gmail.com>
2020-08-11 20:08:36 +0530

    Added method setPlane

--- Plane.h
+++ Plane.h
@@ -5,5 +5,6 @@
 class Plane : public TriangleMesh
 {
 public:
-    Plane(QOpenGLShaderProgram* prog, float xsize, float zsize, int xdivs, int zdivs, float smax = 1.0f, float tmax = 1.0f);
+    Plane(QOpenGLShaderProgram* prog, float xsize, float ysize, int xdivs, int zdivs, float zlevel = 0.0f, float smax = 1.0f, float tmax = 1.0f);
+    void setPlane(QOpenGLShaderProgram* prog, float xsize, float ysize, int xdivs, int zdivs, float zlevel = 0.0f, float smax = 1.0f, float tmax = 1.0f);
 };
