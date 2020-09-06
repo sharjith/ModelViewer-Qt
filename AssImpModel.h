4c81beb233ccbf85e64444995f51a95a2445ebfa
Sharjith Naramparambath <sharjith@gmail.com>
2020-09-06 14:28:15 +0530

    Fixed texture color to RGBA

--- AssImpModel.h
+++ AssImpModel.h
@@ -256,7 +256,7 @@ private:
 
         // Assign texture to ID
         glBindTexture( GL_TEXTURE_2D, textureID );
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, texImage.width(), texImage.height(), 0, GL_RGB, GL_UNSIGNED_BYTE, texImage.bits() );
+        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, texImage.width(), texImage.height(), 0, GL_RGBA, GL_UNSIGNED_BYTE, texImage.bits() );
         glGenerateMipmap( GL_TEXTURE_2D );
 
         // Parameters
