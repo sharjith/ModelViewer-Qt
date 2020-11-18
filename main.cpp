1e30e900aa0167823c6bc86a1dff1a269cecc808
Sharjith Naramparambath <sharjith@gmail.com>
2020-11-18 13:19:39 +0530

    Moved GL info console statements to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
 #include <QStyleFactory>
 #include <QDebug>
+#include <QOpenGLFunctions> 
 
 #include "MainWindow.h"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
@@ -17,5 +19,28 @@ int main(int argc, char** argv)
 
 	MainWindow* mw = MainWindow::mainWindow();
 	mw->showMaximized();
+
+	QOpenGLFunctions glFuncs(QOpenGLContext::currentContext());
+	std::cout << "Renderer: " << glFuncs.glGetString(GL_RENDERER) << '\n';
+	std::cout << "Vendor:   " << glFuncs.glGetString(GL_VENDOR) << '\n';
+	std::cout << "OpenGL Version:  " << glFuncs.glGetString(GL_VERSION) << '\n';
+	std::cout << "Shader Version:   " << glFuncs.glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n"
+		<< std::endl;
+
+	/*
+#ifdef QT_DEBUG
+	int n = 0;
+	glFuncs.glGetIntegerv(GL_NUM_EXTENSIONS, &n);
+	for (int i = 0; i < n; i++)
+	{
+		const char* extension =
+				(const char*)glFuncs.glGetStringi(GL_EXTENSIONS, i);
+		printf("GL Extension %d: %s\n", i, extension);
+	}
+	std::cout << std::endl;
+
+#endif // DEBUG
+*/
+
 	return app.exec();
 }
