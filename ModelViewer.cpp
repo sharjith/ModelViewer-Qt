186520f1aa6692fd38cfb92043a34b8d96052172
Sharjith Naramparambath <sharjith@gmail.com>
2020-12-01 21:54:51 +0530

    Revert "Show selection count in status bar"
    This reverts commit ebf290e18b73a612385f0584c33b4ce7df993176.

--- ModelViewer.cpp
+++ ModelViewer.cpp
@@ -1611,9 +1611,6 @@ void ModelViewer::on_listWidgetModel_itemSelectionChanged()
 				_glWidget->deselect(rowId);
 		}
 		_glWidget->update();
-        int selected = listWidgetModel->selectedItems().count();
-        if(selected)
-            MainWindow::showStatusMessage(QString("Selected %1 objects").arg(selected));
 	}
 }
 
